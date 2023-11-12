5dd4bacd6543982b774902b263b4ba246f0e27d8
daver987 <daveyalexander@outlook.com>
2023-11-11 19:26:37 -0500

    Added new directory structure

---
+++ gui/main_window.py
@@ -0,0 +1,40 @@
+# This file will contain the GUI setup for the main window.
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+    QComboBox,
+    QLabel,
+    QLineEdit,
+    QListWidget,
+    QRadioButton,
+)
+
+# Import other necessary modules
+from modules.file_extensions import (
+    video_basic,
+    video_all,
+    image_basic,
+    image_all,
+    spreadsheet,
+    layout,
+    database,
+    design,
+    markup,
+    pdf_documents,
+    text,
+)
+
+# This function will be moved from main.py
+def update_file_types(category_combo, file_type_combo, presentation=None):
+    # ... (The rest of the update_file_types function code goes here)
+    pass
+
+# ... (The rest of the GUI setup code goes here)
+
+# The main_window function that sets up the GUI
+def main_window():
+    # ... (The rest of the main_window function code goes here)
+    pass
