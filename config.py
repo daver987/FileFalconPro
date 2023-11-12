27fcdf618129f28b419ee64c123dc429cdf71be2
daver987 <daveyalexander@outlook.com>
2023-11-11 20:27:20 -0500

    Format config.py

--- config.py
+++ config.py
@@ -15,13 +15,7 @@ class Config:
 
     @folder_path.setter
     def folder_path(self, value):
-        # Add validation or preprocessing if necessary
         self._folder_path = value
-
-    # Repeat the pattern of property and setter for each configuration attribute
-    # ...
-
-    # The rest of the methods remain unchanged
 
     def set_source_folder_path(self, path):
         self._source_folder_path = path
@@ -36,7 +30,4 @@ class Config:
         return self._dest_folder_path
 
     def update_file_types(self, presentation=None):
-        # This method will be used to update the file types based on the selected category
         pass
-
-    # Additional methods and properties can be added here as needed
